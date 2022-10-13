09d56ac4bff6a94264edb451c21e47a810e7f3bc
Sagar Mittal <mittalsagar006@gmail.com>
2022-10-14 04:57:18 +0530

    learned about components, props & JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-function App() {
+import React from 'react';
+
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Buy Macbook Pro M1 Chip',
+      amount: 1999,
+      date: new Date(2022, 1, 5),
+    },
+    {
+      id: 2,
+      title: 'Buy iPhone 14',
+      amount: 599,
+      date: new Date(2022, 2, 5),
+    },
+    {
+      id: 3,
+      title: 'Buy Apple watch',
+      amount: 299,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: 4,
+      title: 'Buy OnePlus Nord 3',
+      amount: 1999,
+      date: new Date(2022, 3, 5),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
