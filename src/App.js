9ba25b22e65368546e7b6c0583ef83ee1d6b17a9
Sagar Mittal <mittalsagar006@gmail.com>
2022-10-15 22:44:58 +0530

    learned events and state in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -32,6 +33,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
